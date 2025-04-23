b9b5945059fe092da0e322be9e73e9dc3c9c3f3b
Maggie Scalzo <scalzom@roverkids.org>
2025-04-23 14:17:21 -0400

    2.2

--- PlayerPositioning/Assets/Scripts/PlayerController.cs
+++ PlayerPositioning/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projecttilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
