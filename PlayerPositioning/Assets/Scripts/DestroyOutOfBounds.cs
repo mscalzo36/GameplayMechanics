75ebcd6fe201864303f864c69c12600dd8d16daa
Maggie Scalzo <scalzom@roverkids.org>
2025-04-28 13:23:33 -0400

    2.2 completed

--- PlayerPositioning/Assets/Scripts/DestroyOutOfBounds.cs
+++ PlayerPositioning/Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,10 +13,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-
-        }
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        } 
     }
 }
